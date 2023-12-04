dc723c786e9dde6f6c4e985edd0b91f1a998a4fa
vektemok <lunnoa15@gmail.com>
2023-12-05 02:23:58 +0300

    Generate l10n

--- lib/src/features/runner/base/app/material_ctx.dart
+++ lib/src/features/runner/base/app/material_ctx.dart
@@ -4,8 +4,7 @@ import 'package:untitled14/src/common/components/themes/light_theme.dart';
 import 'package:untitled14/src/features/announcement/announcement_publisher/presentation/screens/announcement_publisher_screen.dart';
 import 'package:untitled14/src/features/settings/locale/wrappers/localiztions_wrapper.dart';
 import 'package:untitled14/src/features/settings/theme/wrappers/theme_wrapper.dart';
-
-// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../../../announcement/announcement_publisher/presentation/screens/select_image_screen.dart';
 import '../../../announcement/announcement_viewer/presentation/screens/announcement_viewer_screen.dart';
 
@@ -15,9 +14,9 @@ class MaterialCtx extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // localizationsDelegates: AppLocalizations.localizationsDelegates,
-      // supportedLocales: AppLocalizations.supportedLocales,
-      // locale: LocaleScope.of(context).data.appLocale,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      locale: LocaleScope.of(context).data.appLocale,
       themeMode: ThemeScope.of(context).data.themeMode,
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
